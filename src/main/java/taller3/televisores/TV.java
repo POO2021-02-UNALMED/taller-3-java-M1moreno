07290e4b27c199fefd347e434606ee469acc6660
Michael Moreno Valoyes <80339158+M1moreno@users.noreply.github.com>
2021-11-02 20:52:47 -0500

    Update TV.java

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -78,25 +78,25 @@ public class TV {
 	
 	public void canalUp() {
 		if (estado && canal >= 1 && canal <= 120) {
-			canal++;
+			this.canal++;
 		}
 	}
 	
 	public void canalDown() {
 		if (estado && canal >= 1 && canal <= 120) {
-			canal--;
+			this.canal--;
 		}
 	}
 	
 	public void volumenUp() {
 		if (estado && volumen >= 0 && volumen <= 7) {
-			volumen++;
+			this.volumen++;
 		}
 	}
 	
 	public void volumenDown() {
 		if (estado && volumen >= 0 && volumen <= 7) {
-			volumen--;
+			this.volumen--;
 		}
 	}
 }
